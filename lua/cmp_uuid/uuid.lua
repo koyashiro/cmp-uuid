11a1131e0356efd2eb2d68f898cdd0c7b7c9092e
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-06-07 01:40:06 +0900

    feat: call `math.randomseed()` with `os.time()`

--- lua/cmp_uuid/uuid.lua
+++ lua/cmp_uuid/uuid.lua
@@ -1,11 +1,12 @@
 local M = {}
 
+math.randomseed(os.time())
+
 -- Ref: https://gist.github.com/jrus/3197011
 local function uuid_v4()
-  local random = math.random
   local template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
   return string.gsub(template, "[xy]", function(c)
-    local v = (c == "x") and random(0, 0xf) or random(8, 0xb)
+    local v = (c == "x") and math.random(0, 0xf) or math.random(8, 0xb)
     return string.format("%x", v)
   end)
 end
